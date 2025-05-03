c21e5a9e3d31d43104ae404236e12ad756c0fa24
ttdat <tuandat260104@gmail.com>
2025-05-03 17:45:45 +0700

    thêm thư mục data và bỏ ảnh vào từng folder khác nhau

---
+++ code/code_final/imagetools.py
@@ -0,0 +1,9 @@
+import os
+from PIL import Image
+
+def load_image(image_path):
+    '''
+    Đọc ảnh từ đối tượng cho trước và trả về đối tượng ảnh
+    ARGS: image_path
+    Returns: đối tượng ảnh
+    '''
